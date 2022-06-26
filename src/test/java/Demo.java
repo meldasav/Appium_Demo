bfde522b06bb7541739cfda67ef06fd7510b265d
meldasav <gmeldag83@hotmail.com>
2022-06-26 18:14:05 -0500

    changes added

---
+++ src/test/java/Demo.java
@@ -0,0 +1,56 @@
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class Demo {
+    @Test
+    public void firstTest() throws InterruptedException, MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setCapability(AndroidMobileCapabilityType.PLATFORM_NAME, "android");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "pixel 4 29");
+        caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+        //caps.setCapability("appPackage", "com.android.chrome");
+        //caps.setCapability("appActivity", "com.google.android.apps.chrome.Main");
+
+        /*
+        YOU CAN USE BELOW CODE START APPIUM SERVER WITH NO MANUAL EFFORT
+        AndroidDriver<AndroidElement> driver = null; //new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4773/wd/hub"), dc);;
+        AppiumDriverLocalService service = AppiumDriverLocalService.buildDefaultService();
+        service.start();
+        driver = new AndroidDriver<AndroidElement>(service, dc);
+         */
+
+
+        URL url = new URL("http://127.0.0.1:4723/wd/hub/");
+        //AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(url, caps);
+        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        //driver.findElementById("com.android.chrome:id/search_box_text").sendKeys("TechGlobal");
+
+        //Thread.sleep(10000);
+
+
+
+        //BROWSER TESTING FOR WEB APPLICATION BUT ON MOBILE DEVICES
+        WebDriver driver = new RemoteWebDriver(url, caps);
+
+        driver.get("https://www.carvana.com");
+
+        //driver.findElement(By.name("q")).sendKeys("Appium" + Keys.ENTER);
+
+        Thread.sleep(10000);
+
+        System.out.println("TEST IS HERE!!!");
+        Assert.assertTrue(true);
+    }
+}
